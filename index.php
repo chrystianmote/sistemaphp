db8050231344f15d331f9605da179f62a32412f4
dev <rafa9694@gmail.com>
2020-08-26 16:58:11 -0300

    ajuste no form

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Cadastro</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
 <?php
-if(isset($_POST['salvar'])) 
-{ 
+if (isset($_POST['salvar'])) {
 
     $nome = $_POST['nome'];
     $documento = $_POST['documento'];
@@ -24,14 +24,14 @@ if(isset($_POST['salvar']))
     $servername = "localhost";
     $username = "root";
     $password = "root";
-    $dbname = "cadastro";     
+    $dbname = "cadastro";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        if(strlen($documento) > 11) {
+        if (strlen($documento) > 11) {
             $sql = "INSERT INTO empresa (razao_social, cnpj, email, telefone, logradouro, numero, bairro, cidade, uf)
             VALUES ('$nome' , '$documento', '$email', '$celular', '$endereco', '$numero',' $bairro', '$cidade', '$uf')";
             echo "
@@ -50,25 +50,36 @@ if(isset($_POST['salvar']))
         }
         // use exec() because no results are returned
         $conn->exec($sql);
-        
-      } catch(PDOException $e) {
+
+    } catch (PDOException $e) {
         echo "<br>" . $e->getMessage();
-      }
-      
-      $conn = null;
+    }
+
+    $conn = null;
 }
 ?>
     <div class="page">
         <form class="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <h1>Cadastro</h1>
             <div class="row">
+                <label for="contato" class="typesInputTitle">Categoria :</label>
+            </div>
+            <div class="rowRadio">
+                <input class="radio" id="cpf" type="radio" name="category" checked>
+                <span>Pessoa Física</span>
+            </div>
+            <div class="rowRadio">
+                <input class="radio" id="cnpj" type="radio" name="category">
+                <span>Pessoa Jurídica</span>
+            </div>
+            <div class="row">
                 <div class="group">
-                    <label for="nome">Nome/Razão Social: </label>
-                    <input type="text" name="nome" placeholder="Nome/Razão Social">
+                    <label id="nome" for="nome">Nome: </label>
+                    <input type="text" name="nome" placeholder="Nome">
                 </div>
                 <div class="group">
-                    <label for="documento">Pessoa Física/Júridica: </label>
-                    <input type="text" name="documento" placeholder="Pessoa Física/Júridica">
+                    <label id="documento" for="documento">Pessoa Física: </label>
+                    <input type="text" name="documento" placeholder="Pessoa Física">
                 </div>
             </div>
             <div class="typesInput">
@@ -99,7 +110,7 @@ if(isset($_POST['salvar']))
                        <label for="numero">Numero: </label>
                        <input type="text" name="numero" id="numero" value="0" placeholder="Nº">
                     </div>
-            </div>
+                </div>
                 <div class="row">
                     <div class="group">
                        <label for="bairro">Bairro: </label>
@@ -115,7 +126,12 @@ if(isset($_POST['salvar']))
                        <label for="uf">UF: </label>
                        <input type="text" name="uf" id="uf" placeholder="UF">
                     </div>
+
+
+                <div class="group">
+                       
                 </div>
+               
             </div>
             <div class="row" style="justify-content: center;">
                 <button type="submit" name="salvar">Salvar</button>
@@ -127,5 +143,23 @@ if(isset($_POST['salvar']))
             </div>
         </form>
     </div>
+
+    <script>
+        var category = null;
+        $("input[name='category']").click(function() {
+            category = this.value;
+            if(this.id == 'cpf') {
+               document.getElementById('nome').innerHTML = 'Nome:';
+               document.getElementById('documento').innerHTML = 'Pessoa Física:';
+               $('input[name=nome]').attr('placeholder','Nome');
+               $('input[name=documento]').attr('placeholder','Pessoa Física');
+            } else {
+                document.getElementById('nome').innerHTML = 'Razão Social:';
+                document.getElementById('documento').innerHTML = 'Pessoa Júridica:';
+                $('input[name=nome]').attr('placeholder','Razão Social');
+                $('input[name=documento]').attr('placeholder','Pessoa Júridica');
+            }
+        });
+    </script>
 </body>
 </html>
