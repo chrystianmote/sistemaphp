68dbf45bd56458c6ff65ad112d357aeb4125c431
dev <rafa9694@gmail.com>
2020-08-28 14:48:03 -0300

    Colocando as bibliotecas de bootstrap e jquery para dentro do projeto

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-jQuery(document).ready(function ($){
 
-    $("#documento").mask('000.000.000-00');
-    $("#telefone").mask('(00) 00000-0000');
-    $("#cep").mask('000000-000');
-
+function mudarNomeCampos() {
     $("input[type='radio']").click(function(e) {
         if(this.id == 'cpf') {
             $(".lbname").html("Nome:");
@@ -20,5 +16,40 @@ jQuery(document).ready(function ($){
             $("#documento").val('');
             $("#documento").mask('00.000.000/0000-00');
         }
-    });            
+    });
+}
+
+function cep() {
+    $("#cep").on('blur', function(e) {
+        const cep = $(this).val().replace('-', '');
+
+        if(cep.length != 8) {
+            alert('CEP invalido!');
+            return;
+        }
+
+        $.get(`https://viacep.com.br/ws/${cep}/json/`,function(data) {
+            if(data.erro) {
+                alert('CEP não encontrado!');
+                return;
+            } else {
+                $('#endereco').val(data.logradouro);
+                $('#bairro').val(data.bairro);
+                $('#cidade').val(data.localidade);
+                $('#uf').val(data.uf);
+            } 
+        }
+        )
+    })
+}
+
+jQuery(document).ready(function ($){
+
+    $("#documento").mask('000.000.000-00');
+    $("#telefone").mask('(00) 00000-0000');
+    $("#cep").mask('00000-000');
+
+    mudarNomeCampos();
+    cep();
+    
 });
