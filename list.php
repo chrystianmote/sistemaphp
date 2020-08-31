970dfe4702ec14340bfef88835764c8118297011
dev <rafa9694@gmail.com>
2020-08-31 17:08:47 -0300

    Trocando o css da tabela para o do bootstrap e adicionando os botões de editar e excluir

--- list.php
+++ list.php
@@ -17,19 +17,23 @@
     <div class="list">
         <h1>List</h1>
 
-        <table>
+        <table class="table table-striped">
+            
+            <thead>
+                <tr>
+                    <th scope="col">Nome/Razão Social</th>
+                    <th scope="col">CPF/CNPJ</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Logradouro</th>
+                    <th scope="col">Número</th>
+                    <th scope="col">Bairro</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">UF</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>    
             <tbody>
-            <tr>
-                <th>Nome/Razão Social</th>
-                <th>CPF/CNPJ</th>
-                <th>Email</th>
-                <th>Telefone</th>
-                <th>Logradouro</th>
-                <th>Número</th>
-                <th>Bairro</th>
-                <th>Cidade</th>
-                <th>UF</th>
-            </tr>
             <?php
             $servername = "localhost";
             $username = "root";
@@ -51,18 +55,30 @@
 
                     $linha['telefone'] = $linha['telefone'] ? $linha['telefone'] : ' - ';
                     $linha['logradouro'] = $linha['logradouro'] ? $linha['logradouro'] : ' - ';
-                    $linha['numero'] = $linha['numero'] ? $linha['numero'] : ' - ';
+                    $linha['numero'] = $linha['numero'] ? $linha['numero'] : 'S/D';
                     $linha['bairro'] = $linha['bairro'] ? $linha['bairro'] : ' - ';
                     $linha['cidade'] = $linha['cidade'] ? $linha['cidade'] : ' - ';
                     $linha['uf'] = $linha['uf'] ? $linha['uf'] : ' - ';
-
+                    
                     echo "<td>{$linha['telefone']}</td>";
                     echo "<td>{$linha['logradouro']}</td>";
                     echo "<td>{$linha['numero']}</td>";
                     echo "<td>{$linha['bairro']}</td>";
                     echo "<td>{$linha['cidade']}</td>";
                     echo "<td>{$linha['uf']}</td>";
-
+                    echo '<td class="d-flex">
+                            <a href="" class="btn btn-outline-secondary mr-1">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                         </svg></a>';
+                    echo '<a href="" class="btn btn-outline-danger">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
+                          </a></td>';
+                         
                     echo "</tr>";
                 }
 
@@ -86,6 +102,21 @@
                     echo "<td>{$linha['bairro']}</td>";
                     echo "<td>{$linha['cidade']}</td>";
                     echo "<td>{$linha['uf']}</td>";
+
+                     echo '<td class="d-flex">
+                            <a href="" class="btn btn-outline-secondary mr-1">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                         </svg></a>';
+                    echo '<a href="" class="btn btn-outline-danger">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
+                          </a></td>';
+                         
+                    echo "</tr>";
                 }
 
             } catch (PDOException $e) {
