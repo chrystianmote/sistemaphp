0696acb76cc143ff15977714c003c3f9aac7b2e8
dev <rafa9694@gmail.com>
2020-08-31 15:30:35 -0300

    Colocando select no campo UF e colocando máscara númerica campo número

--- index.php
+++ index.php
@@ -224,7 +224,37 @@ if (isset($_POST['salvar'])) {
                     <div class="row">
                         <label for="uf" class="col-2 col-form-label">UF:</label>
                         <div class="col-10 pl-2">
-                            <input type="text" name="uf" class="form-control" id="uf" placeholder="UF" required>
+                            <select class="custom-select" name="uf" id="uf" required>
+                                <option value="" selected></option>
+                                <option value="AC">Acre</option>
+                                <option value="AL">Alagoas</option>
+                                <option value="AP">Amapá</option>
+                                <option value="AM">Amazonas</option>
+                                <option value="BA">Bahia</option>
+                                <option value="CE">Ceará</option>
+                                <option value="DF">Distrito Federal</option>
+                                <option value="ES">Espírito Santo</option>
+                                <option value="GO">Goiás</option>
+                                <option value="MA">Maranhão</option>
+                                <option value="MT">Mato Grosso</option>
+                                <option value="MS">Mato Grosso do Sul</option>
+                                <option value="MG">Minas Gerais</option>
+                                <option value="PA">Pará</option>
+                                <option value="PB">Paraíba</option>
+                                <option value="PR">Paraná</option>
+                                <option value="PE">Pernambuco</option>
+                                <option value="PI">Piauí</option>
+                                <option value="RJ">Rio de Janeiro</option>
+                                <option value="RN">Rio Grande do Norte</option>
+                                <option value="RS">Rio Grande do Sul</option>
+                                <option value="RO">Rondônia</option>
+                                <option value="RR">Roraima</option>
+                                <option value="SC">Santa Catarina</option>
+                                <option value="SP">São Paulo</option>
+                                <option value="SE">Sergipe</option>
+                                <option value="TO">Tocantins</option>
+                            </select>
+                            <!-- <input type="text" name="uf" class="form-control" id="uf" placeholder="UF" required> -->
                             <div class="invalid-feedback">
                                 Campo obrigatório
                             </div>
--- js/index.js
+++ js/index.js
@@ -84,6 +84,7 @@ function SetMascaras() {
     $("#documento").mask('000.000.000-00');
     $("#telefone").mask('(00) 00000-0000');
     $("#cep").mask('00000-000');
+    $("#numero").mask('#');
 }
 
 function validarDocumento(element) {
